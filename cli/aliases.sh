bc2453ed14430578d546770441931afb2514e564
paul-s1 <77027872+paul-s1@users.noreply.github.com>
2024-10-28 11:52:29 -0400

    add git aliases

---
+++ cli/aliases.sh
@@ -0,0 +1,7 @@
+## Git Aliases/Configs
+# git config --global --add --bool push.autoSetupRemote true
+alias gpush="git push"
+alias gshow="git branch --show-current"
+alias gco="git checkout"
+alias glog="git log"
+alias gitm="git commit -m ${1}"
